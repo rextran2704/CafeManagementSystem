823afa56e9cacb38df01e48d3b609d9178687e4d
rextran2704 <rextran2704@noreply.github.com>
2020-11-14 16:03:54 +0700

    DB Info

---
+++ dao/DatabaseInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeManagementSystem.dao
+{
+    class DatabaseInfo
+    {
+        private static string dbInfo;
+
+        public static string DbInfo()
+        {
+            string filePath = Path.Combine(Environment.CurrentDirectory, "database-info.txt"); 
+            dbInfo = System.IO.File.ReadAllText(filePath);
+            return dbInfo;
+        }
+    }
+}
