f982dd1dc3dc55b0fb909904e1da20b7d985cf6d
rextran2704 <rextran2704@noreply.github.com>
2020-11-16 21:12:20 +0700

    resolve

--- MainForm.cs
+++ MainForm.cs
@@ -26,7 +26,6 @@ namespace CafeManagementSystem
         }
         private void MainForm_Load(object sender, EventArgs e)
         {
-<<<<<<< HEAD
             this.TopMost = true;
             this.FormBorderStyle = FormBorderStyle.None;
             this.WindowState = FormWindowState.Maximized;
@@ -72,37 +71,6 @@ namespace CafeManagementSystem
             orderForm.FormBorderStyle = FormBorderStyle.None;
             orderForm.Dock = DockStyle.Fill;
             orderForm.Show();
-=======
-            panelProductSubmenu.Visible = false;
-            panelReportSubmenu.Visible = false;
-            panelUserSubmenu.Visible = false;
-
-        }
-        void toggleSubmenu(Panel submenu) {
-            if (submenu.Visible == true)
-                submenu.Visible = false;
-            else
-                submenu.Visible = true;
-        }
-        private void btnCurrentUser_Click(object sender, EventArgs e)
-        {
-            toggleSubmenu(panelUserSubmenu);
-        }
-
-        private void btnSanPham_Click(object sender, EventArgs e)
-        {
-            toggleSubmenu(panelProductSubmenu);
-        }
-
-        private void btnBaoCAo_Click(object sender, EventArgs e)
-        {
-            toggleSubmenu(panelReportSubmenu);
-        }
-
-        private void btnTaiKhoan_Click(object sender, EventArgs e)
-        {
-            openChildForm(new FormTaiKhoan());
->>>>>>> f4cd1fe66cabe61a8ae10b4a5abef44a80395bb4
         }
     }
 }
