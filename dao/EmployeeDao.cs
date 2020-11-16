6efeda01e986f8f8df43247632a5ff97cac0a1f0
Phan Chi Bao <phanchibao22@gmail.com>
2020-11-16 10:20:32 +0700

    Add ReceiptDao, ProductDao, EmployeeDao

---
+++ dao/EmployeeDao.cs
@@ -0,0 +1,130 @@
+﻿using CafeManagementSystem.model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace CafeManagementSystem.dao
+{
+    class EmployeeDao
+    {
+        public Employee GetEmployeeByName(string Name)
+        {
+            Employee employee= null;
+            Dao dao = new Dao();
+            string sqlStatement = "SELECT EmployeeID, Gender, Position, StartDate, Address, PhoneNumber FROM Employee WHERE EmployeeName='" + Name + "'";
+            System.Data.SqlClient.SqlDataReader reader = dao.Get(sqlStatement);
+            if (reader.Read())
+            {
+                int id = reader.GetInt32(0);
+                bool gender = reader.GetBoolean(1);
+                string position = reader.GetString(2);
+                DateTime date = reader.GetDateTime(3);
+                string address = reader.GetString(4);
+                string phone = reader.GetString(5);
+                employee = new Employee(id, Name, gender, position, date, address, phone);
+            }
+            dao.Con.Close();
+            return employee;
+        }
+        public List<Employee> GetEmployeeList()
+        {
+            List<Employee> employeeList = new List<Employee>();
+            Dao dao = new Dao();
+            string sqlStatement = "SELECT EmployeeID, EmployeeName, Gender, Position, StartDate, Address, PhoneNumber FROM Employee";
+            System.Data.SqlClient.SqlDataReader reader = dao.Get(sqlStatement);
+            if (reader == null) return null;
+            while (reader.Read())
+            {
+                int id = reader.GetInt32(0);
+                string name = reader.GetString(1);
+                bool gender = reader.GetBoolean(2);
+                string position = reader.GetString(3);
+                DateTime date = reader.GetDateTime(4);
+                string address = reader.GetString(5);
+                string phone = reader.GetString(6);
+                employeeList.Add(new Employee(id, name, gender, position, date, address, phone));
+            }
+            dao.Con.Close();
+            return employeeList;
+        }
+
+        public bool RemoveEmployeeByUsername(string name)
+        {
+            try
+            {
+                int nRows;
+                Dao dao = new Dao();
+                string sqlStatement = "DELETE FROM Employee WHERE EmployeeName=@name";
+                using (SqlCommand command = dao.Remove(sqlStatement))
+                {
+                    command.CommandText = sqlStatement;
+                    command.Parameters.AddWithValue("@name", name);
+                    nRows = command.ExecuteNonQuery();
+                }
+                dao.Con.Close();
+                return nRows > 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+        public bool AddEmployee(Employee employee)
+        {
+            try
+            {
+                int nRows;
+                Dao dao = new Dao();
+                string sqlStatement = "INSERT INTO Employee(EmployeeName, Gender, Position, StartDate, Address, PhoneNumber) VALUES (@name, @gender, @position, @date, @address, @phone)";
+                using (SqlCommand command = dao.Add(sqlStatement))
+                {
+                    command.CommandText = sqlStatement;
+                    command.Parameters.AddWithValue("@name", employee.EmployeeName);
+                    command.Parameters.AddWithValue("@gender", employee.Gender);
+                    command.Parameters.AddWithValue("@position", employee.Position);
+                    command.Parameters.AddWithValue("@date", employee.StartDate);
+                    command.Parameters.AddWithValue("@address", employee.Address);
+                    command.Parameters.AddWithValue("@phone", employee.PhoneNumber);
+                    nRows = command.ExecuteNonQuery();
+                }
+                dao.Con.Close();
+                return nRows > 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        public bool UpdateEmployee(Employee employee)
+        {
+            try
+            {
+                int nRows;
+                Dao dao = new Dao();
+                string sqlStatement = "UPDATE Employee SET Gender=@gender, Position=@position, StartDate = @date, Address = @address, PhoneNumber = @Phone WHERE EmployeeName=@name";
+                using (SqlCommand command = dao.Update(sqlStatement))
+                {
+                    command.CommandText = sqlStatement;
+                    command.Parameters.AddWithValue("@name", employee.EmployeeName);
+                    command.Parameters.AddWithValue("@gender", employee.Gender);
+                    command.Parameters.AddWithValue("@position", employee.Position);
+                    command.Parameters.AddWithValue("@date", employee.StartDate);
+                    command.Parameters.AddWithValue("@address", employee.Address);
+                    command.Parameters.AddWithValue("@phone", employee.PhoneNumber);
+                    nRows = command.ExecuteNonQuery();
+                }
+                dao.Con.Close();
+                return (nRows > 0);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+    }
+}
---
+++ dao/ReceiptDao.cs
@@ -0,0 +1,127 @@
+﻿using CafeManagementSystem.model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace CafeManagementSystem.dao
+{
+    class ReceiptDao
+    {
+        public Receipt GetReceiptByReceiptID(int id)
+        {
+            Receipt Receipt= null;
+            Dao dao = new Dao();
+            string sqlStatement = "SELECT EmployeeID, TableNumber, PrintDate, Total, AdditionalFee FROM Receipt WHERE ReceiptID='" + id + "'";
+            System.Data.SqlClient.SqlDataReader reader = dao.Get(sqlStatement);
+            if (reader.Read())
+            {
+                int emID = reader.GetInt32(0);
+                int tablenumber= reader.GetInt32(1);
+                DateTime date= reader.GetDateTime(2);
+                double total= reader.GetDouble(3);
+                double addFee= reader.GetDouble(4);
+                Receipt = new Receipt(id, emID, tablenumber, date, total, addFee);
+            }
+            dao.Con.Close();
+            return Receipt;
+        }
+        public List<Receipt> GetReceiptList()
+        {
+            List<Receipt> ReceiptList = new List<Receipt>();
+            Dao dao = new Dao();
+            string sqlStatement = "SELECT ReceiptID, EmployeeID, TableNumber, PrintDate, Total, AdditionalFee FROM Receipt";
+            System.Data.SqlClient.SqlDataReader reader = dao.Get(sqlStatement);
+            if (reader == null) return null;
+            while (reader.Read())
+            {
+                int reID = reader.GetInt32(0);
+                int emID = reader.GetInt32(1);
+                int tablenumber = reader.GetInt32(2);
+                DateTime date = reader.GetDateTime(3);
+                double total = reader.GetDouble(4);
+                double addFee = reader.GetDouble(5);
+                ReceiptList.Add(new Receipt(reID, emID, tablenumber, date, total, addFee));
+            }
+            dao.Con.Close();
+            return ReceiptList;
+        }
+
+        public bool RemoveReceiptByID(int id)
+        {
+            try
+            {
+                int nRows;
+                Dao dao = new Dao();
+                string sqlStatement = "DELETE FROM Receipt WHERE ReceiptID=@id";
+                using (SqlCommand command = dao.Remove(sqlStatement))
+                {
+                    command.CommandText = sqlStatement;
+                    command.Parameters.AddWithValue("@id", id);
+                    nRows = command.ExecuteNonQuery();
+                }
+                dao.Con.Close();
+                return nRows > 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+        public bool AddReceipt(Receipt Receipt)
+        {
+            try
+            {
+                int nRows;
+                Dao dao = new Dao();
+                string sqlStatement = "INSERT INTO Receipt(EmployeeID, TableNumber, PrintDate, Total, AdditionalFee) VALUES (@emID, @tablenumber, @date, @total, @addfee)";
+                using (SqlCommand command = dao.Add(sqlStatement))
+                {
+                    command.CommandText = sqlStatement;
+                    command.Parameters.AddWithValue("@emID", Receipt.EmployeeID);
+                    command.Parameters.AddWithValue("@tablenumber", Receipt.TableNumber);
+                    command.Parameters.AddWithValue("@date", Receipt.PrintDate);
+                    command.Parameters.AddWithValue("@total", Receipt.Total);
+                    command.Parameters.AddWithValue("@addfee", Receipt.AdditionalFee);
+                    nRows = command.ExecuteNonQuery();
+                }
+                dao.Con.Close();
+                return nRows > 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        public bool UpdateReceipt(Receipt Receipt)
+        {
+            try
+            {
+                int nRows;
+                Dao dao = new Dao();
+                string sqlStatement = "UPDATE Receipt SET EmployeeID=@emID, TableNumber=@tablenumber, PrintDate= @date, Total= @total, AdditionalFee= @addFee WHERE ReceiptID=@id";
+                using (SqlCommand command = dao.Update(sqlStatement))
+                {
+                    command.CommandText = sqlStatement;
+                    command.Parameters.AddWithValue("@id", Receipt.ReceiptID);
+                    command.Parameters.AddWithValue("@emID", Receipt.EmployeeID);
+                    command.Parameters.AddWithValue("@tablenumber", Receipt.TableNumber);
+                    command.Parameters.AddWithValue("@date", Receipt.PrintDate);
+                    command.Parameters.AddWithValue("@total", Receipt.Total);
+                    command.Parameters.AddWithValue("@addFee", Receipt.AdditionalFee);
+                    nRows = command.ExecuteNonQuery();
+                }
+                dao.Con.Close();
+                return (nRows > 0);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+    }
+}
